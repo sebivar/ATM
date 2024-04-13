3fcad11150ce89bde16ae5acb8c0c123d2a137d2
sebivar <sebivarvea26@gmail.com>
2024-04-13 17:17:58 +0300

    add event listener on enter button

--- PIN-SCREEN/index.js
+++ PIN-SCREEN/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world!')
+const enterButton = document.getElementById('enter-button');
+console.log('enterButton' , enterButton  )
+enterButton?.addEventListener('click' , () => {
+    console.log('You clicked the button!')
+})
+
